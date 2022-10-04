157ec384e1df4a9b7958275d23cd6442c3e2a787
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2022-10-04 14:30:44 -0300

    Create Exercício_05.py

---
+++ Resposta dos Exercícios/Exercício_05.py
@@ -0,0 +1,11 @@
+
+#Resolução 01
+def calculo_imc(peso, altura):
+ print(peso / altura ** 2)
+ calculo_imc(75, 1.68)
+
+ #Resolução 2
+
+#def calculo_imc(peso, altura):
+    #print(peso / altura ** 2)
+    #calculo_imc(peso = 75, altura = 1.68)
