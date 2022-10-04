96c163a99505294499b4422620155388461ad34a
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2022-10-04 14:25:09 -0300

    Create Exemplo_01.py

---
+++ 02 - Exemplos de Códigos/Exemplo_01.py
@@ -0,0 +1,10 @@
+def compara(x,y):
+    if x == y:
+        print("Números Iguais: ")
+    else:
+         print("Números Diferentes: ")
+
+num1 = input("Digite um valor: ")
+num2 = input("Digite outro valor: ")
+compara(num1,num2)
+
