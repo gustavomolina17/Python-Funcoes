07b2ced446108c997f2809c78b4d0ca3f953fa26
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2022-10-04 14:25:12 -0300

    Create Exemplo_02.py

---
+++ 02 - Exemplos de Códigos/Exemplo_02.py
@@ -0,0 +1,11 @@
+def compara(x):
+    if x == 0:
+        print("Zero")
+    elif x % 2 == 0:
+        print("Par")
+    else:
+         print("Impar")
+
+n1 = int(input("Digite um valor: "))
+compara(n1)
+
