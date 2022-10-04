7d6eeb30cf61510b1f1fd9b7cc936029bbca268f
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2022-10-04 14:25:19 -0300

    Create Função_02.py

---
+++ 02 - Exemplos de Códigos/Função_02.py
@@ -0,0 +1,7 @@
+#definindo a função
+def subtracao(a,b): #a e b são os parâmetros da função
+    print("A subtração é: ",a-b)
+
+#chamando a função subtração
+subtracao(5,2) #5 e 2 são argumentos da função.
+
